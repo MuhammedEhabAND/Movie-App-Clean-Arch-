aad2f7ba585d005c0a7dba18242912bd2001af71
MuhammedEhabAND <muhammedehab726@gmail.com>
2024-01-14 06:42:20 +0200

    adding structure of the app

---
+++ lib/core/utils/Constants.dart
@@ -0,0 +1,7 @@
+class Constants {
+  static const String baseUrl = "https://api.themoviedb.org/3";
+  static const String apiKey = "c771bd60547309885734a5303de5517e";
+  static const String apiAccessToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNzcxYmQ2MDU0NzMwOTg4NTczNGE1MzAzZGU1NTE3ZSIsInN1YiI6IjY1YTM2MWYwYTM0OTExMDEyNDBhMTQ3ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FI_P78qq5zlLcswIwTV0scd-3FRS4rL2aG_l3VoU_bw";
+
+
+}
